e1a3e3bc836c86fe32bef10a1fcebe9576d73490
gibbonsmartin <32617706+gibbonsmartin@users.noreply.github.com>
2021-01-22 20:09:31 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
-let angle1 = 0
 function on_button_pressed_a() {
-    
-    angle1 += 1
     servos.P0.setAngle(0)
     led.toggle(0, 0)
 }
 
 function on_button_pressed_b() {
-    
-    angle1 -= 1
     servos.P0.setAngle(90)
     led.toggle(4, 0)
 }
 
 function on_button_pressed_s() {
-    
-    angle1 = 0
     servos.P0.setAngle(45)
+    led.toggle(1, 0)
+}
+
+function on_button_pressed_u() {
+    servos.P1.setAngle(0)
     led.toggle(2, 0)
+}
+
+function on_button_pressed_d() {
+    servos.P1.setAngle(90)
+    led.toggle(3, 0)
 }
 
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function on_uart_data_received() {
@@ -28,10 +31,15 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function on_
         on_button_pressed_b()
     } else if (data == "s") {
         on_button_pressed_s()
-    }
-    
+    } else if (data == "u") {
+        on_button_pressed_u()
+    } else if (data == "d") {
+        on_button_pressed_d()
+    }    
 })
 input.onButtonPressed(Button.A, on_button_pressed_a)
 input.onButtonPressed(Button.B, on_button_pressed_b)
 pins.servoSetPulse(AnalogPin.P0, 1500)
+pins.servoSetPulse(AnalogPin.P1, 1500)
 servos.P0.setAngle(45)
+servos.P1.setAngle(45)
